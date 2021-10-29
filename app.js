331e8af9ec6b7a02a8ab1ee18dd0e42709658210
Donielle20 <donielle_hope@yahoo.com>
2021-10-29 15:20:18 -0500

    Edited Files

--- app.js
+++ app.js
@@ -13,11 +13,7 @@ window.onload = function()
 
         httpRequest.onreadystatechange = processName;
         httpRequest.open('POST', url);
-        // Notice for the POST request we are setting the Content-Type
         httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        // Notice for the POST request we are passing in our name parameter as part
-        // of the send method. Also ensure you encode any special characters using
-        // encodeURIComponent()
         httpRequest.send('name=' + encodeURIComponent(name));
 
         function processName() {
@@ -31,29 +27,6 @@ window.onload = function()
               }
             }
           }
-
-    //     var httpRequest = new XMLHttpRequest(); 
-
-    //     var url = "superheroes.php"; 
-    //     httpRequest.onreadystatechange = doSomething; 
-    //     httpRequest.open('GET', url); 
-    //     httpRequest.send();
-
-    //     function doSomething() 
-    //     { 
-    //         if (httpRequest.readyState === XMLHttpRequest.DONE) 
-    //         { 
-    //             if (httpRequest.status === 200) 
-    //             { 
-    //                 var response = httpRequest.responseText; 
-    //                 document.getElementById("result").innerHTML = response;
-    //             } 
-    //             else 
-    //             { 
-    //                 alert('There was a problem with the request.'); 
-    //             } 
-    //         } 
-    //     }
     }
     );
 
