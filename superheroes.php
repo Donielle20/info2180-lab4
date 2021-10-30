25678a34443482836fdc70d08191c8b4f8ca4ec1
Donielle20 <donielle_hope@yahoo.com>
2021-10-30 17:51:58 -0500

    Edited Files

--- superheroes.php
+++ superheroes.php
@@ -67,13 +67,10 @@ $superheroes = [
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-    //echo "Your name is: <strong>" . $_POST['name'] . "</strong>";
-    // echo "<h3>" . $superheroes[0]['name'] . "</h3>";
-    // echo "<h3>" . $_POST['name'] . "</h3>";
     $count = 0;
     foreach ($superheroes as $superhero)
     {
-        if ($superhero['name'] == $_POST['name'] or $superhero['alias'] == $_POST['name'])
+        if ($superhero['name'] == strip_tags($_POST['name']) or $superhero['alias'] == strip_tags($_POST['name']))
         {
             $count = $count + 1;
             echo "<h3>" . $superhero['alias'] . "</h3>";
@@ -81,12 +78,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
             echo "<p>" . $superhero['biography'] . "</p>";
         }
     } 
-    if ($count == 0 and $_POST['name'] != "")
+    if ($count == 0 and strip_tags($_POST['name']) != "")
     {
         echo '<h3 style="color:red;">' . "SUPERHERO NOT FOUND" . "</h3>";
     }  
 
-    if ($_POST['name'] == "")
+    if (strip_tags($_POST['name']) == "")
     {
         echo "<ul>";
         foreach ($superheroes as $superhero)
